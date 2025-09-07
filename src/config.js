84f4cefd6ec24068b41a95ec695a35598f8b075b
Zaid Alshamaa <zshamaa20@gmail.com>
2025-09-07 12:21:55 +0300

    edit baseurl

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
-export const BaseUrl = "http://127.0.0.1:8000/api/v1";
-export const ImageUrl = "http://127.0.0.1:8000/";
+// export const BaseUrl = "http://127.0.0.1:8000/api/v1";
+// export const ImageUrl = "http://127.0.0.1:8000/";
 
-// export const BaseUrl = "https://46c0e275f9d4.ngrok-free.app/api/v1";
-// export const ImageUrl = "https://46c0e275f9d4.ngrok-free.app";
+
+export const BaseUrl = "https://8f133bd878b0.ngrok-free.app/api/v1";
+export const ImageUrl = "https://8f133bd878b0.ngrok-free.app";
 
 // export const BaseUrl = "https://c3adj.ciroue.com/api/v1";
 // export const ImageUrl = "https://c3adj.ciroue.com";
